36413e4ae63b7b9d889ac68e630f86648e45bb6f
Rubem Fridolino Christ Neto <56802722+RubemNto@users.noreply.github.com>
2021-07-26 02:17:48 -0300

    Add files via upload

---
+++ MonogameTiled/MonogameTrain/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonogameTrain
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Map _GameMap;
+        private int ScreenScaler = 2;
+        private int TileSize = 16;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferHeight = 16 * 20 *ScreenScaler;
+            _graphics.PreferredBackBufferWidth = 16 * 25 *ScreenScaler;
+            _graphics.ApplyChanges();
+            _GameMap = new Map("Content/JsonFiles/platformer video world.json",this,TileSize,ScreenScaler);            
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            //GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin(samplerState:SamplerState.PointClamp);
+            _GameMap.DrawMap(_spriteBatch,ScreenScaler);
+            _spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonogameTiled/MonogameTrain/Map.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MonogameTrain
+{
+    class Map
+    {
+        private List<Layer> MapLayers;
+        public Map(string MapPath,Game1 game,int tileSize,int ScreenScaler = 1)
+        {
+            MapLayers = new List<Layer>();
+            using (StreamReader JsonFile = File.OpenText(MapPath)) 
+            {
+                JObject MapJson = (JObject)JToken.ReadFrom(new JsonTextReader(JsonFile));
+                JArray LayersJson = (JArray)MapJson["layers"];
+                foreach (JObject Layers in LayersJson)
+                {
+                    string Json = "{\n"+$"data:{Layers["data"].ToString()},\nname:'{Layers["name"].ToString()}',"+"\n}";
+                    Layer layer = JsonConvert.DeserializeObject<Layer>(Json);
+                    layer.LoadTileset(game,tileSize);
+                    MapLayers.Add(layer);                    
+                }                
+            }
+        }
+
+        public void DrawMap(SpriteBatch spriteBatch, int ScreenScaler = 1) 
+        {
+            foreach (Layer layer in MapLayers)
+            {
+                layer.DrawLayer(spriteBatch,ScreenScaler);
+            }
+        }
+
+    }
+}
